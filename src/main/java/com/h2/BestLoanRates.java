1905bd0b47cc184228c34a6ea7341a57845e7d3a
Nicolasf740 <82244505+Nicolasf740@users.noreply.github.com>
2021-04-26 14:18:20 -0300

    Merge remote-tracking branch 'origin/master'

--- src/main/java/com/h2/BestLoanRates.java
+++ src/main/java/com/h2/BestLoanRates.java
@@ -22,7 +22,7 @@ public class BestLoanRates {
             System.out.println("No available rates for term: " + loanTermInYears + "years");
 
         } else {
-            System.out.println("Best available rate: " + getRates(loanTermInYears) + "%");
+            System.out.println("Best Available rate: " + getRates(loanTermInYears) + "%");
         }
         scanner.close();
     }
