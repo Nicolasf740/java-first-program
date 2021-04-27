62d84b797291f1c4076f4ed8e5c4d6fdca2565c4
Nicolasf740 <82244505+Nicolasf740@users.noreply.github.com>
2021-04-27 17:52:16 -0300

    opknapfnnonnng

--- src/main/java/com/h2/MortgageCalculator.java
+++ src/main/java/com/h2/MortgageCalculator.java
@@ -38,9 +38,9 @@ public class MortgageCalculator {
     }
 
     public static void main(String[] args) {
-        long loanAmount = Long.parseLong(args[0]);
-        int termInYears = Integer.parseInt(args[1]);
-        float annualRate = Float.parseFloat(args[2]);
+        long loanAmount = Ultilities.getLongValue(args[0]);
+        int termInYears = Ultilities.getIntValue(args[1]);
+        float annualRate = Ultilities.getFloatValue(args[2]);
 
         MortgageCalculator calculator = new MortgageCalculator(loanAmount, termInYears, annualRate);
         calculator.calculateMonthlyPayment();
